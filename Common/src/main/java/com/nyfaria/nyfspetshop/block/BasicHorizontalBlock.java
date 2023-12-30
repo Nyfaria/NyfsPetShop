476148fef2249c642da4274cbb3057514eff6856
Nyfaria DragonMoose <kinaria3324@gmail.com>
2023-12-30 12:06:27 -0600

    treat, begging, crate texture, follow pet item
    Took 1 hour 58 minutes

---
+++ Common/src/main/java/com/nyfaria/nyfspetshop/block/BasicHorizontalBlock.java
@@ -0,0 +1,29 @@
+package com.nyfaria.nyfspetshop.block;
+
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import org.jetbrains.annotations.Nullable;
+
+public class BasicHorizontalBlock extends HorizontalDirectionalBlock {
+    public BasicHorizontalBlock(Properties $$0) {
+        super($$0);
+        this.registerDefaultState(
+                this.stateDefinition.any().setValue(FACING, Direction.NORTH)
+        );
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> $$0) {
+        $$0.add(FACING);
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext $$0) {
+        return this.defaultBlockState().setValue(FACING, $$0.getHorizontalDirection().getOpposite());
+    }
+}
--- Common/src/main/java/com/nyfaria/nyfspetshop/init/BlockInit.java
+++ Common/src/main/java/com/nyfaria/nyfspetshop/init/BlockInit.java
@@ -1,6 +1,7 @@
 package com.nyfaria.nyfspetshop.init;
 
 import com.nyfaria.nyfspetshop.Constants;
+import com.nyfaria.nyfspetshop.block.BasicHorizontalBlock;
 import com.nyfaria.nyfspetshop.block.GroomingStation;
 import com.nyfaria.nyfspetshop.block.PetBowl;
 import com.nyfaria.nyfspetshop.registration.RegistrationProvider;
@@ -42,7 +43,7 @@ public class BlockInit {
     public static final RegistryObject<Block> PET_BOWL_RED = registerPetBowl("pet_bowl_red", () -> new PetBowl(DyeColor.RED, Block.Properties.copy(Blocks.RED_WOOL).noOcclusion()));
     public static final RegistryObject<Block> PET_BOWL_BLACK = registerPetBowl("pet_bowl_black", () -> new PetBowl(DyeColor.BLACK, Block.Properties.copy(Blocks.BLACK_WOOL).noOcclusion()));
     public static final RegistryObject<Block> GROOMING_STATION = registerBlock("grooming_station", () -> new GroomingStation(Block.Properties.copy(Blocks.CRAFTING_TABLE).noOcclusion()));
-    public static final RegistryObject<Block> CRATE = registerBlock("crate", () -> new Block(Block.Properties.copy(Blocks.CRAFTING_TABLE).noOcclusion()));
+    public static final RegistryObject<Block> CRATE = registerBlock("crate", () -> new BasicHorizontalBlock(Block.Properties.copy(Blocks.CRAFTING_TABLE).noOcclusion()));
 
     public static <T extends Block> RegistryObject<T> registerPetBowl(String name, Supplier<T> block) {
         RegistryObject<T> reg = registerBlock(name, block, b -> () -> new BlockItem(b.get(), ItemInit.getItemProperties(Rarity.COMMON)));
