321944ce2b965f892056b1a71b64a7fc33ba0453
Nyfaria DragonMoose <kinaria3324@gmail.com>
2024-01-04 10:32:41 -0600

    setting up fabric
    Took 12 minutes

--- Forge/src/main/java/com/nyfaria/nyfspetshop/event/ClientModEvents.java
+++ Forge/src/main/java/com/nyfaria/nyfspetshop/event/ClientModEvents.java
@@ -17,6 +17,7 @@ import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraft.client.renderer.entity.ThrownItemRenderer;
 import net.minecraft.client.renderer.entity.player.PlayerRenderer;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.api.distmarker.Dist;
@@ -57,23 +58,9 @@ public class ClientModEvents {
 
     @SubscribeEvent
     public static void onEntityRenderers(EntityRenderersEvent.RegisterRenderers event) {
-        event.registerEntityRenderer(EntityInit.BALL.get(), ThrownItemRenderer::new);
-        event.registerEntityRenderer(EntityInit.SHELTIE.get(), context -> new PetRenderer<>(context, new DefaultedEntityGeoModel<>(new ResourceLocation(Constants.MODID, "sheltie"), true)));
-        event.registerEntityRenderer(EntityInit.ENGLISH_COCKER_SPANIEL.get(), context -> new PetRenderer<>(context, new DefaultedEntityGeoModel<>(new ResourceLocation(Constants.MODID, "english_cocker_spaniel"), true)));
-        event.registerEntityRenderer(EntityInit.SUPER_MUTT.get(), context -> new PetRenderer<>(context, new DefaultedEntityGeoModel<>(new ResourceLocation(Constants.MODID, "super_mutt"), true)));
-        event.registerEntityRenderer(EntityInit.SABLE_HUSKY.get(), context -> new PetRenderer<>(context, new DefaultedEntityGeoModel<BaseDog>(new ResourceLocation(Constants.MODID, "husky"), true).withAltTexture(new ResourceLocation(Constants.MODID, "sable_husky"))));
-        event.registerEntityRenderer(EntityInit.BLACK_AND_WHITE_HUSKY.get(), context -> new PetRenderer<>(context, new DefaultedEntityGeoModel<BaseDog>(new ResourceLocation(Constants.MODID, "husky"), true).withAltTexture(new ResourceLocation(Constants.MODID, "black_and_white_husky"))));
-
-        event.registerEntityRenderer(EntityInit.CALICO.get(), context -> new PetRenderer<>(context, new DefaultedEntityGeoModel<BaseCat>(new ResourceLocation(Constants.MODID, "base_cat"), true).withAltTexture(new ResourceLocation(Constants.MODID, "calico"))));
-        event.registerEntityRenderer(EntityInit.AMERICAN_SHORTHAIR.get(), context -> new PetRenderer<>(context, new DefaultedEntityGeoModel<BaseCat>(new ResourceLocation(Constants.MODID, "base_cat"), true).withAltTexture(new ResourceLocation(Constants.MODID, "american_shorthair"))));
-        event.registerEntityRenderer(EntityInit.BLACK_TUXEDO.get(), context -> new PetRenderer<>(context, new DefaultedEntityGeoModel<BaseCat>(new ResourceLocation(Constants.MODID, "base_cat"), true).withAltTexture(new ResourceLocation(Constants.MODID, "black_tuxedo"))));
-        event.registerEntityRenderer(EntityInit.BROWN_TUXEDO_MUNCHKIN.get(), context -> new PetRenderer<>(context, new DefaultedEntityGeoModel<BaseCat>(new ResourceLocation(Constants.MODID, "munchkin"), true).withAltTexture(new ResourceLocation(Constants.MODID, "brown_tuxedo_munchkin"))));
-
-        event.registerEntityRenderer(EntityInit.GOLD_DASHED_PARROT.get(), context -> new PetRenderer<>(context, new DefaultedEntityGeoModel<BaseBird>(new ResourceLocation(Constants.MODID, "base_bird"), true).withAltTexture(new ResourceLocation(Constants.MODID, "gold_dashed_parrot"))));
-        event.registerEntityRenderer(EntityInit.WHITE_STRIPED_PARROT.get(), context -> new PetRenderer<>(context, new DefaultedEntityGeoModel<BaseBird>(new ResourceLocation(Constants.MODID, "base_bird"), true).withAltTexture(new ResourceLocation(Constants.MODID, "white_striped_parrot"))));
-        event.registerEntityRenderer(EntityInit.RED_ACCENT_ALBINO_PARROT.get(), context -> new PetRenderer<>(context, new DefaultedEntityGeoModel<BaseBird>(new ResourceLocation(Constants.MODID, "base_bird"), true).withAltTexture(new ResourceLocation(Constants.MODID, "red_accent_albino_parrot"))));
-        event.registerEntityRenderer(EntityInit.TROPICAL_PARROT.get(), context -> new PetRenderer<>(context, new DefaultedEntityGeoModel<BaseBird>(new ResourceLocation(Constants.MODID, "base_bird"), true).withAltTexture(new ResourceLocation(Constants.MODID, "tropical_parrot"))));
-
+        CommonClientClass.getRenderers().forEach(
+                record -> event.registerEntityRenderer((EntityType) record.type().get(), record.renderer())
+        );
         event.registerBlockEntityRenderer(BlockInit.BIRD_CAGE_BE.get(), (context)->new BirdCageRenderer());
     }
 
